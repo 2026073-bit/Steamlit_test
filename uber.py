1b62953e5fdae2c94e0fb40ed7d0b9c378b8d97f
2026073-bit <2026073@student.cct.ie>
2026-05-18 11:47:32 +0100

    Update uber.py

--- uber.py
+++ uber.py
@@ -1,9 +1,13 @@
+import streamlit as st
 import pandas as pd
-import numpy as np 
-import streamlit as sl
 
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+ 
 
-sl.title('Uber picked in New York City') 
 
 
 
